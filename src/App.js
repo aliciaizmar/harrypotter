649cedc8d63f351697f67de0eed04b2298ea5274
Alicia I.M <younglust@gmail.com>
2021-01-28 22:54:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
   }
 
   getUser() {
-    fetch('http://hp-api.herokuapp.com/api/characters')
+    fetch('https://hp-api.herokuapp.com/api/characters')
       .then(response => response.json())
       .then(data => {
         const newData = data.map((item, index) => {
